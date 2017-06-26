5c5f139908025b4e3ca2f71631196ef5a16b2e34
Willi Ballenthin <willi.ballenthin@gmail.com>
2017-06-26 16:26:27 -0400

    tests: use new netnode interface

--- tests/test_netnode.py
+++ tests/test_netnode.py
@@ -3,10 +3,32 @@ from fixtures import *
 import datetime
 import binascii
 
-import idb.netnode
-
 
 def test_name(kernel32_idb):
-    pass
+    root = kernel32_idb.netnode('Root Node')
+    assert root.name() == 'Root Node'
+
+    nn = kernel32_idb.netnode(0x401000)
+    assert nn.name() == ''
 
 
+def test_root_node(kernel32_idb):
+    root = kernel32_idb.netnode('Root Node')
+    assert root is not None
+
+    root = root.deref()
+    assert root is not None
+
+    assert root.altval(idb.netnode.ROOT_INDEX.VERSION) == 695
+    assert root.supstr(idb.netnode.ROOT_INDEX.VERSION_STRING) == '6.95'
+    assert root.altval(idb.netnode.ROOT_INDEX.OPEN_COUNT) == 1
+
+    ts = root.altval(idb.netnode.ROOT_INDEX.CREATED)
+    ts = datetime.datetime.utcfromtimestamp(ts)
+    assert ts.isoformat() == '2017-06-20T22:31:34'
+
+    assert root.altval(idb.netnode.ROOT_INDEX.CRC) == 0xdf9bdf12
+
+    md5 = root.supval(idb.netnode.ROOT_INDEX.MD5)
+    md5 = binascii.hexlify(md5).decode('ascii')
+    assert md5 == '00bf1bf1b779ce1af41371426821e0c2'
