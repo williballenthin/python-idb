49409fea705c3de5ed85b8de667413c2457ffe4b
William Ballenthin <william.ballenthin@fireeye.com>
2017-12-10 18:50:49 -0500

    netnode: correctly handle signed netnode index when emitting the key

--- idb/netnode.py
+++ idb/netnode.py
@@ -59,8 +59,10 @@ def make_key(nodeid, tag=None, index=None, wordsize=4):
 
         if index is None:
             return b'.' + struct.pack('>' + wordformat + 'c', nodeid, tag)
-        else:
+        elif index < 0:
             return b'.' + struct.pack('>' + wordformat + 'c' + wordformat.lower(), nodeid, tag, index)
+        else:
+            return b'.' + struct.pack('>' + wordformat + 'c' + wordformat, nodeid, tag, index)
     else:
         raise ValueError('unexpected type of nodeid: ' + str(type(nodeid)))
 
