55ff49044507fb3563a09469d7a360735da8a062
William Ballenthin <william.ballenthin@fireeye.com>
2017-12-10 19:41:31 -0500

    add test for github issue #22

---
+++ tests/test_issue22.py
@@ -0,0 +1,17 @@
+import os.path
+
+import idb
+
+
+def test_issue22():
+    '''
+    demonstrate that functions found at addresses with the high bit set are no problem.
+    see github issue #22 for the backstory.
+    '''
+    cd = os.path.dirname(__file__)
+    idbpath = os.path.join(cd, 'data', 'highaddr', 'highaddr.idb')
+
+    with idb.from_file(idbpath) as db:
+        api = idb.IDAPython(db)
+        assert len(api.idautils.Functions()) == 1
+        assert api.idautils.Functions()[0] == 0xF7FFFFFF
