3b19444f016734065fa80a7e53be37060beda802
William Ballenthin <william.ballenthin@fireeye.com>
2017-12-23 22:26:45 -0500

    tests: add regression test for issue 30

---
+++ tests/test_issue30.py
@@ -0,0 +1,20 @@
+import os.path
+
+import idb
+
+
+def test_issue32():
+    '''
+    demonstrate get_func_cmt().
+    see github issue #30 for the backstory.
+    '''
+    cd = os.path.dirname(__file__)
+    idbpath = os.path.join(cd, 'data', 'issue30', 'issue30.i64')
+
+    with idb.from_file(idbpath) as db:
+        api = idb.IDAPython(db)
+
+        assert api.idc.GetCommentEx(0x401598, 0) == 'local cmt'
+        assert api.idc.GetCommentEx(0x401598, 1) == ''
+        assert api.idc.get_func_cmt(0x401598, 0) == 'rep cmt'
+        assert api.idc.get_func_cmt(0x401598, 1) == 'rep cmt'
