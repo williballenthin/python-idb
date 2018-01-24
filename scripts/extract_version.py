500dabcfbabfbfb95fadbf2735122a4f820aa747
William Ballenthin <william.ballenthin@fireeye.com>
2018-01-24 10:41:34 -0500

    add script to extract IDA Pro version

---
+++ scripts/extract_version.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+'''
+Extract the IDA Pro version used to create the IDB.
+
+author: Willi Ballenthin
+email: willi.ballenthin@gmail.com
+'''
+import sys
+import logging
+
+import hexdump
+import argparse
+
+import idb
+import idb.netnode
+
+
+logger = logging.getLogger(__name__)
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(description="Extract the IDA Pro version used to create the IDB.")
+    parser.add_argument("idbpath", type=str,
+                        help="Path to input idb file")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="Enable debug logging")
+    parser.add_argument("-q", "--quiet", action="store_true",
+                        help="Disable all output but errors")
+    args = parser.parse_args(args=argv)
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.getLogger().setLevel(logging.DEBUG)
+    elif args.quiet:
+        logging.basicConfig(level=logging.ERROR)
+        logging.getLogger().setLevel(logging.ERROR)
+    else:
+        logging.basicConfig(level=logging.INFO)
+        logging.getLogger().setLevel(logging.INFO)
+
+    with idb.from_file(args.idbpath) as db:
+        root = idb.analysis.Root(db)
+        print(root.version)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
