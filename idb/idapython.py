e6ff1dc5ab6b522d08731a40119884aa65775083
William Ballenthin <william.ballenthin@fireeye.com>
2018-06-15 13:02:32 -0600

    idapython: add method memoizer

--- idb/idapython.py
+++ idb/idapython.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
+import weakref
+import functools
 import collections
 
 import six
@@ -9,6 +11,25 @@ import idb.analysis
 
 
 logger = logging.getLogger(__name__)
+
+
+
+# via: https://stackoverflow.com/a/33672499/87207
+def memoized_method(*lru_args, **lru_kwargs):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapped_func(self, *args, **kwargs):
+            # We're storing the wrapped method inside the instance. If we had
+            # a strong reference to self the instance would never die.
+            self_weak = weakref.ref(self)
+            @functools.wraps(func)
+            @functools.lru_cache(*lru_args, **lru_kwargs)
+            def cached_method(*args, **kwargs):
+                return func(self_weak(), *args, **kwargs)
+            setattr(self, func.__name__, cached_method)
+            return cached_method(*args, **kwargs)
+        return wrapped_func
+    return decorator
 
 
 def is_flag_set(flags, flag):
