8a2d0533e1dde9fd6178aafdaafcde4035cc8b9f
William Ballenthin <william.ballenthin@fireeye.com>
2018-06-19 14:42:05 -0600

    move shim into idb module

--- scripts/run_ida_script.py
+++ scripts/run_ida_script.py
@@ -8,51 +8,14 @@ email: willi.ballenthin@gmail.com
 import sys
 import os.path
 import logging
-import importlib.abc
-import importlib.util
 
 import argparse
 
 import idb
+import idb.shim
 
 
 logger = logging.getLogger(__name__)
-
-
-class HookedImporter(importlib.abc.MetaPathFinder, importlib.abc.Loader):
-    def __init__(self, hooks=None):
-        self.hooks = hooks
-
-    def find_spec(self, name, path, target=None):
-        logger.info('hooked importer: find-spec: %s', name)
-        if name not in self.hooks:
-            return None
-
-        spec = importlib.util.spec_from_loader(name, self)
-        return spec
-
-    def create_module(self, spec):
-        # req'd in 3.6
-        logger.info('hooked importer: create-module: %s', spec.name)
-        module = importlib.util._Module(spec.name)
-        mod = self.hooks[spec.name]
-        for attr in dir(mod):
-            if attr.startswith('__'):
-                continue
-            module.__dict__[attr] = getattr(mod, attr)
-        return module
-
-    def exec_module(self, module):
-        # module is already loaded (imported by line `import idb` above),
-        # so no need to re-execute.
-        #
-        # req'd in 3.6.
-        return
-
-    def install(self):
-        logger.info('install')
-        sys.meta_path.insert(0, self)
-        print(sys.meta_path)
 
 
 def main(argv=None):
@@ -97,22 +60,7 @@ def main(argv=None):
         else:
             screenea = list(sorted(idb.analysis.Segments(db).segments.keys()))[0]
 
-        api = idb.IDAPython(db, ScreenEA=screenea)
-
-        hooks = {
-            'idc': api.idc,
-            'idaapi': api.idaapi,
-            'idautils': api.idautils,
-            'ida_funcs': api.ida_funcs,
-            'ida_bytes': api.ida_bytes,
-            'ida_netnode': api.ida_netnode,
-            'ida_nalt': api.ida_nalt,
-            'ida_name': api.ida_name,
-            'ida_entry': api.ida_entry,
-        }
-
-        importer = HookedImporter(hooks=hooks)
-        importer.install()
+        hooks = idb.shim.install(db, ScreenEA=screenea)
 
         # update sys.path to point to directory containing script.
         # so scripts can import .py files in the same directory.
