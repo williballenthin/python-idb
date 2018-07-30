ccb453c6c9f2459c697f5612479dee80c2ed6249
William Ballenthin <william.ballenthin@fireeye.com>
2018-07-30 11:02:41 -0600

    shim: add support for py2

--- idb/shim.py
+++ idb/shim.py
@@ -1,54 +1,96 @@
 import sys
-if sys.version_info[0] < 3:
-    raise NotImplementedError('Python 3+ only for shimming the import system to work like IDA Pro.')
+if sys.version_info[0] == 2:
+    import imp
+    import sys
+    import logging
 
-import types
-import logging
-import importlib.abc
-import importlib.util
-
-import idb
+    import idb
 
 
-logger = logging.getLogger(__name__)
+    logger = logging.getLogger(__name__)
 
 
+    class HookedImporter(object):
+        # ref: https://www.python.org/dev/peps/pep-0302/#specification-part-1-the-importer-protocol
 
-class HookedImporter(importlib.abc.MetaPathFinder, importlib.abc.Loader):
-    def __init__(self, hooks=None):
-        self.hooks = hooks
+        def __init__(self, hooks=None):
+            super(HookedImporter, self).__init__()
+            self.hooks = hooks
 
-    def find_spec(self, name, path, target=None):
-        if name not in self.hooks:
-            return None
+        def find_module(self, fullname, path=None):
+            logger.info('find_module: fullname: %s, path=%s', fullname, path)
+            if fullname not in self.hooks:
+                return None
 
-        spec = importlib.util.spec_from_loader(name, self)
-        return spec
+            return self
 
-    def create_module(self, spec):
-        # req'd in 3.6
-        logger.info('hooking import: %s', spec.name)
+        def load_module(self, fullname):
+            logger.info('load_module: fullname: %s', fullname)
 
-        module = types.ModuleType(spec.name)
-        module.__loader__ = self
-        module.__package__ = ''
+            mod = self.hooks[fullname]
+            newmod = sys.modules.setdefault(fullname, imp.new_module(fullname))
+            newmod.__file__ = sys.modules[mod.__module__].__file__
+            newmod.__loader__ = self
+            newmod.__package__ = ''
 
-        mod = self.hooks[spec.name]
-        for attr in dir(mod):
-            if attr.startswith('__'):
-                continue
-            module.__dict__[attr] = getattr(mod, attr)
-        return module
+            for attr in dir(mod):
+                if attr.startswith('__'):
+                    continue
+                newmod.__dict__[attr] = getattr(mod, attr)
+            return newmod
 
-    def exec_module(self, module):
-        # module is already loaded (imported by line `import idb` above),
-        # so no need to re-execute.
-        #
-        # req'd in 3.6.
-        return
+        def install(self):
+            sys.meta_path.insert(0, self)
 
-    def install(self):
-        sys.meta_path.insert(0, self)
+
+elif sys.version_info[0] == 3:
+    import sys
+    import types
+    import logging
+    import importlib.abc
+    import importlib.util
+
+    import idb
+
+
+    logger = logging.getLogger(__name__)
+
+
+    class HookedImporter(importlib.abc.MetaPathFinder, importlib.abc.Loader):
+        def __init__(self, hooks=None):
+            self.hooks = hooks
+
+        def find_spec(self, name, path, target=None):
+            if name not in self.hooks:
+                return None
+
+            spec = importlib.util.spec_from_loader(name, self)
+            return spec
+
+        def create_module(self, spec):
+            # req'd in 3.6
+            logger.info('hooking import: %s', spec.name)
+
+            module = types.ModuleType(spec.name)
+            module.__loader__ = self
+            module.__package__ = ''
+
+            mod = self.hooks[spec.name]
+            for attr in dir(mod):
+                if attr.startswith('__'):
+                    continue
+                module.__dict__[attr] = getattr(mod, attr)
+            return module
+
+        def exec_module(self, module):
+            # module is already loaded (imported by line `import idb` above),
+            # so no need to re-execute.
+            #
+            # req'd in 3.6.
+            return
+
+        def install(self):
+            sys.meta_path.insert(0, self)
 
 
 def install(db, ScreenEA=None):
