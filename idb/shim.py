c6a9667ad0280d9825a41317f7e105db97b1fbea
invano <cozziemanuele@gmail.com>
2019-05-21 22:37:49 +0200

    shim: fix the HookedImporter to load __EA64__ also on py2

--- idb/shim.py
+++ idb/shim.py
@@ -33,7 +33,7 @@ if sys.version_info[0] == 2:
             newmod.__package__ = ""
 
             for attr in dir(mod):
-                if attr.startswith("__"):
+                if attr.startswith("__") and attr != "__EA64__":
                     continue
                 newmod.__dict__[attr] = getattr(mod, attr)
             return newmod
