36e13979e77680c1fed6f401ceb715b1a5206ed8
billow <billow.fun@gmail.com>
2020-07-17 15:52:59 +0800

    Fix @slow deprecation error

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,22 @@
+import pytest
+
+
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true", help="run slow tests")
 
     parser.addoption("--rundebug", action="store_true", help="run debug tests")
+
+
+# from https://docs.pytest.org/en/latest/example/simple.html#control-skipping-of-tests-according-to-command-line-option
+def pytest_configure(config):
+    config.addinivalue_line("markers", "slow: mark test as slow to run")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--runslow"):
+        # --runslow given in cli: do not skip slow tests
+        return
+    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    for item in items:
+        if "slow" in item.keywords:
+            item.add_marker(skip_slow)
